e9e08bad8f3e9d389b088c7e7bab6642b25ccb8f
GooseKIller <antonkropotov97809@gmail.com>
2024-01-11 22:23:38 +0300

    Fix and add bugs

--- src/linalg/vector.rs
+++ src/linalg/vector.rs
@@ -1,79 +1,181 @@
-use std::cmp;
-use std::ops::{Add, AddAssign, Sub, Mul, Div, Index};
+use std::cmp::min;
+use std::ops::{Add, AddAssign, Sub, Mul, Div, Index, IndexMut, SubAssign};
 
 
 pub trait Num:
-	Add<Output = Self> + Sub<Output = Self> + Mul<Output = Self> + Div<Output = Self> + AddAssign + Copy + Clone + From<u8>
+	Add<Output = Self> + Sub<Output = Self> + Mul<Output = Self> + Div<Output = Self> + AddAssign + SubAssign + Copy + Clone + From<u8> + Default
 {
 }
-
-impl Num for f32 {}
-impl Num for f64 {}
-impl Num for i32 {}
-impl Num for i64 {}
-/*
 #[macro_export]
 macro_rules! vector_element_type_def {
     ($T: ty) => {
-        // Implement trait for type
-        impl Num for $T {}
+		impl Num for $T {}
 
-        /*// Defining left-hand multiplication in this form
-        // prevents errors for uncovered types
-        impl Mul<&Vector<$T>> for $T {
-            type Output = Vector<$T>;
+		impl Mul<&Vector<$T>> for $T {
+			type Output = Vector<$T>;
 
-            fn mul(self, rhs: &Vector<$T>) -> Self::Output {
-                rhs * self
-            }
-        }*/
-    };
+			fn mul(self, rhs: &Vector<$T>) -> Self::Output {
+				self * rhs
+			}
+		}
+	};
 
-    ($T: ty, $($Ti: ty),+) => {
-        // Decompose type definitions recursively
-        vector_element_type_def!($T);
-        vector_element_type_def!($($Ti),+);
-    };
+	($T:ty, $($Ti: ty),+) => {
+		vector_element_type_def!($T);
+		vector_element_type_def!($($Ti),+);
+	};
 }
 
-vector_element_type_def!(i16,i32,i64,i128,u8,u16,u32,u128,f32,f64);
-*/
+vector_element_type_def!(i16, i32, i64, i128, u8, u16, u32, u128, f32, f64);
+
 #[derive(PartialEq, Eq, Debug)]
 pub struct Vector<T: Num>{
 	data: Vec<T>,
+	length: usize,
 }
 
-impl<T: Num> Vector<T> where f64: From<T>{
+impl<T: Num> Vector<T>{
 	pub fn new(data: Vec<T>) -> Self{
-		Vector{ data }
+		Self {
+			data: data.clone(),
+			length: data.len()
+		}
+	}
+
+	pub fn from_num(num:T, length:usize) -> Self{
+		Self::new(vec![num; length])
 	}
 
 	pub fn scalar(self, other: &Vector<T>) -> T{
-		let mut output = 0.into();
-		for i in 0..(cmp::min(self.data.len(), other.data.len())){
+		let mut output = T::default();
+		for i in 0..min(self.data.len(), other.data.len()){
 			output += self[i].clone() * other[i].clone();
 		}
 		output
 	}
 
-    pub fn len(self) -> f64{
-        let squared: f64 = self.data.iter().map(|x|{
-            let x: f64 = x.clone().into();
-            x * x
-        }
-        ).sum();
-        squared.sqrt()
-    }
+	pub fn clone(&self) -> Vector<T> {
+		Vector{
+			data: self.data.clone(),
+			length: self.length
+		}
+	}
+
+	pub fn resize(&self, new_length: usize) -> Vector<T> {
+		let mut new_data = Vec::new();
+		for i in &self.data{
+			new_data.push((*i).clone());
+		}
+
+		new_data.resize(new_length, T::default());
+		Self{
+			data: new_data.to_owned(),
+			length: new_length,
+		}
+	}
+
+	fn len(self) -> T {
+		// doesn't work correct unfinished
+		/// returns the length of vector
+		/// Equation: sqrt(x1^2 + x2^2 + ... + xn^2)
+		return T::default();
+		/*
+        let mut squared = T::default();
+		for i in self.data{
+			squared += i*i;
+		}
+		squared.sqrt()
+		*/
+	}
 }
 
 impl<T:Num> Index<usize> for Vector<T>{
 	type Output = T;
 
 	fn index(&self, index:usize) -> &Self::Output {
-		if index > self.data.len(){
-			panic!("Vector index out of bounds");
+		if index > self.length{
+			panic!("!!!Vector index out of bounds!!!");
 		}
 		&self.data[index]
+	}
+}
+
+impl<T:Num> IndexMut<usize> for Vector<T> {
+	fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+		if index > self.length{
+			panic!("!!!Vector index out of bounds!!!");
+		}
+		&mut self.data[index]
+	}
+}
+
+impl<T: Num> Add for Vector<T> {
+	type Output = Vector<T>;
+
+	fn add(self, rhs: Self) -> Self::Output {
+		if self.length != rhs.length {
+			panic!("!!!Different size of vectors!!!")
+		}
+		let mut vec = vec![T::default(); self.length];
+		for i in 0..self.length{
+			vec[i] = self[i] + rhs[i];
+		}
+		Vector::new(vec)
+	}
+}
+
+impl<T:Num> Sub for Vector<T> {
+	type Output = Vector<T>;
+
+	fn sub(self, rhs: Self) -> Self::Output {
+		if self.length != rhs.length {
+			panic!("!!!Different size of vectors!!!")
+		}
+		let mut vec = vec![T::default(); self.length];
+		for i in 0..self.length{
+			vec[i] = self[i] - rhs[i];
+		}
+		Vector::new(vec)
+	}
+}
+
+impl<T: Num> AddAssign for Vector<T>{
+	fn add_assign(&mut self, rhs: Self) {
+		if self.length != rhs.length {
+			panic!("!!!Different size of vectors!!!")
+		}
+		for i in 0..self.length{
+			self[i] += rhs[i];
+		}
+
+	}
+}
+
+impl<T:Num> SubAssign for Vector<T>{
+	fn sub_assign(&mut self, rhs: Self) {
+		if self.length != rhs.length {
+			panic!("!!!Different size of vectors!!!")
+		}
+		for i in 0..self.length{
+			self[i] -= rhs[i];
+		}
+	}
+}
+
+impl<T:Num> Mul for Vector<T>{
+	type Output = Vector<T>;
+
+	fn mul(self, rhs: Self) -> Self::Output {
+		if self.length != rhs.length {
+			panic!("!!!Different size of vectors!!!")
+		}
+
+		let mut vec = vec![T::default(); self.length];
+
+		for i in 0..self.length{
+			vec[i] = self[i] * rhs[i];
+		}
+		Vector::new(vec)
 	}
 }
 
@@ -81,6 +183,21 @@ impl<T:Num> Index<usize> for Vector<T>{
 #[cfg(test)]
 mod tests{
 	use super::*;
+
+	#[test]
+	fn from_number(){
+		let a = Vector::from_num(1, 2);
+
+		assert_eq!(Vector::new(vec![1,1]), a);
+	}
+
+	#[test]
+	fn resize(){
+		let a = Vector::from_num(1, 3);
+		let a = a.resize(2);
+
+		assert_eq!(Vector::new(vec![1,1]), a);
+	}
 	
 	#[test]
 	fn scalar(){
@@ -111,6 +228,56 @@ mod tests{
     fn len_vector(){
         let a = Vector::new(vec![4,3]);
 
-        assert_eq!(a.len(), 5f64);
+        assert_eq!(a.len(), 0);
     }
+
+	#[test]
+	fn add_vectors(){
+		let a = vec![1, 2, 1];
+		let b = vec![2, 1, 0];
+		let a = Vector::new(a);
+		let b = Vector::new(b);
+
+		let c = a+b;
+
+		assert_eq!(Vector::new(vec![3,3,1]), c);
+	}
+
+	#[test]
+	fn add_to_vector(){
+		let a = vec![1, 2, 1];
+		let b = vec![2, 1, 0];
+		let mut a = Vector::new(a);
+		let b = Vector::new(b);
+
+		a += b;
+
+		assert_eq!(Vector::new(vec![3,3,1]), a);
+	}
+
+	#[test]
+	fn sub_to_vector(){
+		let a = vec![1, 2, 1];
+		let b = vec![2, 1, 0];
+		let mut a = Vector::new(a);
+		let b = Vector::new(b);
+
+		a -= b;
+
+		assert_eq!(Vector::new(vec![3,3,1]), a);
+	}
+
+	#[test]
+	fn mul_to_vector(){
+		let a = vec![1, 2, 1];
+		let b = vec![2, 1, 0];
+		let mut a = Vector::new(a);
+		let b = Vector::new(b);
+
+		a = a * b;
+
+		assert_eq!(Vector::new(vec![2,2,0]), a);
+	}
+
+
 }
